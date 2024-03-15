c1ca61a3afd92676ed489c9adbcfadc01539efc0
PedroHenrique1s <phsouzahd15@gmail.com>
2024-03-15 17:54:44 -0300

    construction(Unirp.poo) - Primeiro exercicio feito com orientação a objeto

---
+++ 03- Terceiro Semestre/Poo/Unirp.poo/Unirp.poo/Curso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unirp.poo
+{
+    internal class Curso
+    {
+        public string Nome { private get; set; }
+        public int QuantidadeSemestre { private get; set; }
+        public int CargaHoraria { private get; set; }
+        public Instituicao Faculdade { private get; set; }
+        public Curso() { }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine($"{Nome}\n{QuantidadeSemestre}\n{CargaHoraria}\n");
+            Faculdade.MostrarDados();
+        }
+    }
+}
---
+++ 03- Terceiro Semestre/Poo/Unirp.poo/Unirp.poo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Unirp.poo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Criando a instituição
+        Instituicao unirp = new Instituicao();
+        unirp.Nome = "UNIRP";
+        unirp.DataFundacao = "01/01/1996";
+        unirp.QuantidadeCursos = 15;
+        unirp.AreaAtuacao = "TI";
+        unirp.QuantidadeAnos = 58;
+
+        //Mostrar dados
+        unirp.MostrarDados();
+
+        //Criando o curso de ciência da Computação
+        Curso ciencia = new Curso();
+        ciencia.Nome = "Ciência da Computação";
+        ciencia.Faculdade = unirp;
+        ciencia.CargaHoraria = 3500;
+        ciencia.QuantidadeSemestre = 8;
+
+        //Mostrar dados
+        ciencia.MostrarDados();
+    }
+}
